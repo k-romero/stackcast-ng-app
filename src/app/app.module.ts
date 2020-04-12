a32d9e3c67f84c0ae577973d0ea532e8def00c80
k-romero <imkevinromero@gmail.com>
2020-04-11 20:04:40 -0400

    build components for video, login, upload etc. and styling for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Router, RouterModule, Routes} from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { VideosComponent } from './videos/videos.component';
+import { UploadComponent } from './upload/upload.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {FormsModule} from "@angular/forms";
+
+const appRoutes :Routes = [
+  {
+    path: 'videos',
+    component: VideosComponent
+  },
+  {
+    path: 'upload',
+    component: UploadComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    // default path
+    path:'',
+    component: VideosComponent,
+    pathMatch:'full'
+  },
+  {
+    // ** for any routes that dont exist
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    VideosComponent,
+    UploadComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true}),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
