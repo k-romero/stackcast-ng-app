ddbca9c524a1998120c312ff9c07f553c8e29715
k-romero <imkevinromero@gmail.com>
2020-04-12 02:39:26 -0400

    complete styling for login and sign up pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userModel:UserViewModel = {
+    userName: '',
+    password: '',
+    isConnected: true
+  };
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  createUser(): void{
+    let url = "http://localhost:8080/zc-video-app/users/create";
+    this.http.post(url,this.userModel).subscribe(
+      res => {
+        location.reload();
+      },
+      err => {
+        alert("An error has occurred while creating user")
+      }
+    );
+  }
+
 }
+
+export interface UserViewModel {
+  userName:string;
+  password:string;
+  isConnected: boolean;
+}
+
 
 
